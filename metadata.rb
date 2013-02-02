44b69f5506835a37bb60875dd290b2359b1251d8
Jonas Pfenniger <jonas@pfenniger.name>
2013-02-02 15:04:40 +0000

    metadata cleanup

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,16 @@
 name             "build-service"
 maintainer       "Jonas Pfenniger"
-maintainer_email "jonas@pfenniger.name"
+maintainer_email "jonas@mediacore.com"
 license          "MIT"
 description      "A specialized version of Jenkins for deploys"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
-%w(machine-base jenkins nginx).each { |cb| depends cb }
+%w[
 
-%w(ubuntu).each do |os|
-	supports os
-end
+machine-base
+jenkins
+nginx
+
+].each { |cb| depends cb }
+
+supports 'ubuntu'
