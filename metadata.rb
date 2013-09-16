8803b463c940fc852aebaec13e59907f39f2211b
Jonas Pfenniger <zimbatm@zimbatm.com>
2013-09-16 10:54:35 -0700

    Make foodcritic happy

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,9 @@ license          "MIT"
 description      "A specialized version of Jenkins for deploys"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
-%w[
-  base
-  jenkins
-  nginx
-].each { |cb| depends cb }
+
+depends          "base"
+depends          "jenkins"
+depends          "nginx"
 
 supports 'ubuntu'
